d3e88b761d40cfb20a1a68027682a14f687d0f04
Twig Nguyen <twig@genscripts.net>
2012-04-08 22:04:14 +1000

    - added CommentManager.valid() - added Comment.__unicode__()

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.comments.models import Comment as DjangoComment
+from django.contrib.comments.managers import CommentManager
+
+
+
+class CommentManager(CommentManager):
+    def valid(self):
+        return self.filter(is_removed = False, is_public = True)
 
 
 class Comment(DjangoComment):
@@ -8,3 +15,6 @@ class Comment(DjangoComment):
     """
     class Meta:
         proxy = True
+
+    def __unicode__(self):
+        return "%s: %s" % (self.user, self.comment)
